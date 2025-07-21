d053fc21af4430f017dd1214ae319affeee52126
Helen Ho <helenho2k1@gmail.com>
2025-07-21 01:35:41 -0700

    title section (rect still needs work)

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import AcciIcon from './assets/bunny-drawings/Web-Icon.png'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Accila</h1>
+    <>
+
+      <div className="title-container">
+        <img src={AcciIcon} className="title-icon"/>
+        <div>
+          <h1>Accila</h1>
+          <h3>Software Dev + Tech Artist</h3>
+        </div>
+      </div>
+
+      <div className="pink-rect"></div>
+
+    </>
   )
 }
 
