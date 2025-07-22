ba5b30bb10efff272f9c3b47e329403a0fd338ac
Helen Ho <helenho2k1@gmail.com>
2025-07-22 00:09:31 -0700

    cleaned up pick rect background

--- src/Page-Format.jsx
+++ src/Page-Format.jsx
@@ -5,19 +5,19 @@ import { useState, useEffect } from 'react'
 function PageFormat({ children }) {
   return (
     <div>
-      <div className="title-container">
-        <img src={AcciIcon} className="title-icon" />
-        <div>
-          <h1>Accila</h1>
-          <h3>Software Dev + Tech Artist</h3>
+       <div className="title-wrapper">
+            <div className="title-container">
+                <img src={AcciIcon} className="title-icon" />
+                <div>
+                <h1>Accila</h1>
+                <h3>Software Dev + Tech Artist</h3>
+                </div>
+            </div>
         </div>
-      </div>
 
-      <div className="pink-rect"></div>
-
-      <main>
-        {children}
-      </main>
+        <main>
+            {children}
+        </main>
     </div>
   )
 }
