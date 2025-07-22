e417396d600a31c8d2e939c4bc2d7341ed4554d3
Helen Ho <helenho2k1@gmail.com>
2025-07-22 15:46:46 -0700

    made description w/ dynamic sizing

--- src/Page-Format.jsx
+++ src/Page-Format.jsx
@@ -22,9 +22,10 @@ function PageFormat({ children }) {
 
         <Description
           texts={[
-            <b>Welcome!</b>,
-            "Feel free to explore my website!",
-            "This is another line of text"
+            <b>Hi! I’m Helen — or Accila/Acci online!</b>,
+            <i>She/Her - 23 - 9/29</i>,
+            "I’m a new grad software developer and tech artist who likes making fun and cute tech! Whether it’s games, websites, or any creative projects, I enjoy blending code with art and bringing ideas to life.",
+            "If you’re interested in working together or just want to chat, feel free to reach out!"
           ]}
         />
 
