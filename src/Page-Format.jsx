516623d42972f0139f23efa02e32effdafaa56c9
Helen Ho <helenho2k1@gmail.com>
2025-07-22 14:34:03 -0700

    adjusted navbar mobile

--- src/Page-Format.jsx
+++ src/Page-Format.jsx
@@ -2,7 +2,9 @@ import './Page-Format.css'
 import './components/NavBar'
 import AcciIcon from './assets/bunny-drawings/Web-Icon.png'
 import { useState, useEffect } from 'react'
+
 import Navbar from './components/NavBar'
+import Description from './components/Description'
 
 function PageFormat({ children }) {
   return (
@@ -11,13 +13,14 @@ function PageFormat({ children }) {
             <div className="title-container">
                 <img src={AcciIcon} className="title-icon" />
                 <div>
-                <h1>Accila</h1>
-                <h3>Software Dev + Tech Artist</h3>
+                  <h1>Accila</h1>
+                  <h3>Software Dev + Tech Artist</h3>
                 </div>
             </div>
         </div>
 
         <Navbar />
+        <Description />
 
         <main>
             {children}
---
+++ src/components/Description.jsx
@@ -0,0 +1,14 @@
+import './Descrption.css'
+
+import PageFormat from './Page-Format.jsx'
+
+function Description() {
+  return (
+    <>
+        <div>
+        </div>
+    </>
+  )
+}
+
+export default Description
