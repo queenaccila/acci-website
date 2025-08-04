057b8d7e09d44f134c96d8ea797c9db6e4f672d2
Helen Ho <helenho2k1@gmail.com>
2025-08-03 22:11:51 -0700

    new github build - 8/3/25

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'docs'
-  }, 
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    build: {
+      outDir: 'docs'
+    },
+    plugins: [react()],
+  }
 })
